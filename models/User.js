29f38d1de0ff735b7387b48011d98519d573c717
Wanessa Dias <130778028+diascw@users.noreply.github.com>
2024-04-30 19:56:44 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,29 @@
 // models/User.js
 const Sequelize = require('sequelize');
 const sequelize = require('../sequelize');
+const bcrypt = require('bcrypt');
 
 const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
+  username: {
+    type: Sequelize.STRING,
+    unique: true, 
+    allowNull: false 
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false 
+  }
+}, {
+  hooks: {
+    async beforeCreate(user) {
+     
+      if (!user.password) {
+        throw new Error(' a senha é obrigatória!');
+      }
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      user.password = hashedPassword;
+    }
+  }
 });
 
 module.exports = User;
